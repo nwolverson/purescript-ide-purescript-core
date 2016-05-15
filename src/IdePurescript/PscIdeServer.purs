af3f9fe1e8b8f54af587832492911144d5f73d32
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2016-05-15 17:32:17 +0100

    Start-server related changes

--- src/IdePurescript/PscIdeServer.purs
+++ src/IdePurescript/PscIdeServer.purs
@@ -1,17 +1,16 @@
 module IdePurescript.PscIdeServer where
 
-import Prelude (const, bind, pure, ($), (++), (==), show, Unit, unit)
-import Data.Maybe(Maybe(..))
-import Data.Either (either)
+import Prelude
+import PscIde.Server as S
+import Control.Monad.Aff (Aff, attempt)
+import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Aff (Aff, attempt, later', makeAff)
-import Control.Monad.Aff.AVar (AVAR)
-import Control.Monad.Aff.Par (Par(Par), runPar)
-import Control.Alt ((<|>))
-import Node.ChildProcess (CHILD_PROCESS, ChildProcess, Exit(Normally), onClose, onError, defaultSpawnOptions, spawn)
+import Data.Either (either)
+import Data.Maybe (Maybe(..))
 import IdePurescript.PscIde (cwd) as PscIde
-import PscIde (NET, quit)
+import Node.ChildProcess (CHILD_PROCESS, ChildProcess)
+import PscIde (NET)
 
 data ServerStartResult =
     CorrectPath
@@ -29,16 +28,11 @@ startServer exe port rootPath = do
 
   launchServer = do
     liftEff $ log "Starting psc-ide-server"
-    cp <- liftEff $ spawn exe ["-p", show port] (defaultSpawnOptions { cwd = Just rootPath })
-    let handleErr = makeAff $ \_ succ -> do
-                      onError cp (\_ -> succ $ StartError "psc-ide-server error")
-                      onClose cp (\exit -> case exit of
-                        (Normally 0) -> succ Closed
-                        (Normally n) -> succ $ StartError $ "Error code returned: "++ show n
-                        _ -> succ $ StartError "Other close error"
-                      )
-
-    runPar (Par handleErr <|> Par (later' 100 $ pure $ Started cp))
+    r <$> S.startServer exe port (Just rootPath)
+    where
+      r (S.Started cp) = Started cp
+      r (S.Closed) = Closed
+      r (S.StartError s) = StartError s
 
   gotPath workingDir =
     liftEff $ if workingDir == rootPath then
@@ -52,6 +46,4 @@ startServer exe port rootPath = do
 
 -- | Stop a psc-ide server. Currently implemented by asking it nicely, but potentially by killing it if that doesn't work...
 stopServer :: forall eff. Int -> ChildProcess -> Aff (cp :: CHILD_PROCESS, net :: NET | eff) Unit
-stopServer port cp = do
-  res <- quit port
-  pure unit
+stopServer port cp = S.stopServer port
